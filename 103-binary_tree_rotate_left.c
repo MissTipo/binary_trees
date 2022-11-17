a9e4394714a2865162e5cc7230723952d18d105c
Isquare007 <akibrahiim2019@gmail.com>
2022-11-17 22:04:30 +0000

    message

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -6,22 +6,19 @@
  *
  * Return: a pointer to the new root node of the tree once rotated
  */
-
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-	binary_tree_t *tmp, *aux;
-	
-	tmp = NULL;
-	if (tree == NULL || tree->right == NULL)
+	if (tree == NULL)
 		return (NULL);
-	if (tree->right->left != NULL)
-		tmp = tree->right->left;
-	aux = tree->right;
-	tree->right->left = tree;
 	tree->parent = tree->right;
-	tree->right->parent = NULL;
-	tree->right = tmp;
-	if (tmp != NULL)
-		tmp->parent = tree;
-	return (aux);
+	if (tree->right)
+	{
+		tree->right = tree->right->left;
+		tree->parent->left = tree;
+		tree->parent->parent = NULL;
+		if (tree->right)
+			tree->right->parent = tree;
+		return (tree->parent);
+	}
+	return (tree);
 }
